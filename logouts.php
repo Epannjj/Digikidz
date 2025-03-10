991294e592f9db1a68dd4f4cc867f45be21202ba
Epannjj <stvnshndrynt@gmail.com>
2025-03-10 11:32:35 +0700

    10 maret

--- logouts.php
+++
@@ -1,8 +0,0 @@
-<?php
-//logout
-session_start();
-session_unset();
-session_destroy();
-header("location:logins.php");
-
-?>
--- siswa/ubah_password.php
+++ siswa/ubah_password.php
@@ -1,46 +1,35 @@
 <form method="POST">
     <label for="old_password">Password Lama:</label>
     <input type="password" name="old_password" id="old_password" required><br>
-
     <label for="new_password">Password Baru:</label>
     <input type="password" name="new_password" id="new_password" required><br>
-
     <label for="confirm_password">Konfirmasi Password Baru:</label>
     <input type="password" name="confirm_password" id="confirm_password" required><br>
-
     <input type="submit" name="submit" value="Ubah Password">
     <?php
-    session_start();  // Memulai session untuk memverifikasi login
-    include '../db.php';  // Pastikan Anda sudah menyertakan koneksi database
-    
-    // Mengecek apakah pengguna sudah login
+    session_start();
+    include '../db.php';
+
     if (!isset($_SESSION["nama"])) {
         header("Location: login.php");
         exit();
     }
     if (isset($_POST['submit'])) {
-        // Mengambil data yang diinputkan oleh pengguna
         $old_password = $_POST['old_password'];
         $new_password = $_POST['new_password'];
         $confirm_password = $_POST['confirm_password'];
-
-        // Ambil data pengguna yang login
         $username = $_SESSION["nama"];
         $query = mysqli_query($db, "SELECT * FROM siswa WHERE nama = '$username'");
         $user = mysqli_fetch_assoc($query);
 
-        // Verifikasi password lama
-        if (password_verify($old_password, $user['password'])) {
-            // Pastikan password baru dan konfirmasi password cocok
+        if ($old_password === $user['password']) {
             if ($new_password == $confirm_password) {
-                // Hash password baru sebelum menyimpannya
-                $hashed_new_password = password_hash($new_password, PASSWORD_DEFAULT);
-
-                // Update password di database
+                $hashed_new_password = $new_password;
                 $update_query = mysqli_query($db, "UPDATE siswa SET password = '$hashed_new_password' WHERE nama = '$username'");
 
                 if ($update_query) {
                     echo "Password berhasil diubah!";
+                    header("Location:siswa.php");
                 } else {
                     echo "Terjadi kesalahan, coba lagi.";
                 }
